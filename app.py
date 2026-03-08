d47e2a823512f78ba8d5a736c0c1bdcc5777f516
VikranthMaster <vikrantht32@gmail.com>
2026-03-08 20:51:39 +0530

    added grp and single

--- app.py
+++ app.py
@@ -16,7 +16,22 @@ def home():
 def run():
     try:
         data = requests.get("https://mun-dat.onrender.com/registrations")
+        data1 = requests.get("https://mun-dat.onrender.com/groups")
         df = pd.DataFrame(data.json())
+        df2 = pd.DataFrame(data1.json())
+
+        max_participants = df2["participants"].apply(len).max()
+
+        for i in range(max_participants):
+            df2[f"p{i+1}_name"] = df2["participants"].apply(lambda x: x[i]["name"] if len(x) > i else "")
+            # df2[f"p{i+1}_phone"] = df2["participants"].apply(lambda x: x[i]["phone"] if len(x) > i else "")
+            # df2[f"p{i+1}_email"] = df2["participants"].apply(lambda x: x[i]["email"] if len(x) > i else "")
+            df2[f"p{i+1}_preference"] = df2["participants"].apply(lambda x: x[i]["preference"] if len(x) > i else "")
+            df2[f"p{i+1}_portfolio1"] = df2["participants"].apply(lambda x: x[i]["portfolio1"] if len(x) > i else "")
+            df2[f"p{i+1}_portfolio2"] = df2["participants"].apply(lambda x: x[i]["portfolio2"] if len(x) > i else "")
+            df2[f"p{i+1}_ipRole"] = df2["participants"].apply(lambda x: x[i]["ipRole"] if len(x) > i else "")
+
+        df2 = df2.drop(columns=["participants"])
 
         if "_id" in df.columns:
             df = df.drop(columns=["_id"])
@@ -35,10 +50,12 @@ def run():
 
         client = gspread.authorize(creds)
         sheet = client.open("MUN_Registrations").sheet1
+        sheet2 = client.open("MUN_Group").sheet1
 
         set_with_dataframe(sheet, df)
+        set_with_dataframe(sheet2, df2)
 
-        return "<a href='https://docs.google.com/spreadsheets/d/1jOBWzZ54qjeUrcsDbVlm7tTx1MBqEfhm6ZNREy6rlVE/edit?gid=0#gid=0'>Link to Google Sheets</a>"
+        return "<a href='https://docs.google.com/spreadsheets/d/1jOBWzZ54qjeUrcsDbVlm7tTx1MBqEfhm6ZNREy6rlVE/edit?gid=0#gid=0'>Link for Individual Registrations</a><br><a href='https://docs.google.com/spreadsheets/d/1lvWxZpEQMb2ujbeEzJ4chdEnicnz5XuZWWRFJC3CZdo/edit?gid=0#gid=0'>Link for Group Registrations</a>"
 
     except Exception as e:
         return f"Error: {str(e)}"
